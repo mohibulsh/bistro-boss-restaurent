59c9ae46a3a50e2160c4bf85cc9b8a74ae4c122c
akalaminamin <alamin@hypescout.co>
2023-05-31 19:26:07 +0600

    add the btn

--- src/ShopPages/ShopCart.jsx
+++ src/ShopPages/ShopCart.jsx
@@ -4,13 +4,14 @@ const ShopCart = ({iteam}) => {
     const {name,recipe,image,price}=iteam;
     console.log(iteam)
     return (
-        <div className="card w-96 bg-base-100 shadow-xl">
+        <div className="card w-96 bg-base-100 shadow-xl relative ">
             <figure><img src={image} alt={name} /></figure>
-            <div className="card-body">
+            <p className='bg-slate-800 absolute right-0 me-4 mt-4 px-2 rounded font-bold'>${price}</p>
+            <div className="card-body flex flex-col items-center ">
                 <h2 className="card-title">{name}</h2>
                 <p>{recipe}</p>
                 <div className="card-actions justify-end">
-                    <button className="btn btn-primary">Add to Cart</button>
+                    <button className="btn btn-outline text-white border-0 border-b-4 bg-slate-200 text-black">Add to Cart</button>
                 </div>
             </div>
         </div>
