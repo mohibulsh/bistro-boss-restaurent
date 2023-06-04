240c7496cdbe212c9b200ce96000bfd948ffb436
akalaminamin <alamin@hypescout.co>
2023-06-04 20:14:08 +0600

    add the dashboard

--- src/Layout/Dashboard.jsx
+++ src/Layout/Dashboard.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import { FaBookOpen, FaCalendarAlt, FaHome, FaRev, FaShoppingCart } from 'react-icons/fa';
-import {  MdPayments } from "react-icons/md";
+import { MdOutlineContactMail, MdPayments } from "react-icons/md";
 import { Link, Outlet } from 'react-router-dom';
+import { AiOutlineMenu} from "react-icons/ai";
 
 const Dashboard = () => {
     return (
@@ -14,16 +15,21 @@ const Dashboard = () => {
 
             </div>
             <div className="drawer-side">
-                <label htmlFor="my-drawer-2" 
-                className="drawer-overlay"></label> 
+                <label htmlFor="my-drawer-2"
+                    className="drawer-overlay"></label>
                 <ul className="menu p-4 w-80 h-full bg-base-200">
                     {/* Sidebar content here */}
-                    <li><Link to='/dashboard/usehome'> <FaHome/> USER HOME</Link></li>
-                    <li><Link to='/dashboard/reservation'> <FaCalendarAlt/> RESERVATION</Link></li>
-                    <li><Link to='/dashboard/payment'><MdPayments/> PAYMENT HISTORY</Link></li>
-                    <li><Link to='/dashboard/mycarts'><FaShoppingCart/> MY CART</Link></li>
-                    <li><Link to='/dashboard/review'><FaRev/> REVIEW</Link></li>
-                    <li><Link to='/dashboard/booking'><FaBookOpen/> MY BOOKING</Link></li>
+                    <li><Link to='/dashboard/usehome'> <FaHome /> USER HOME</Link></li>
+                    <li><Link to='/dashboard/reservation'> <FaCalendarAlt /> RESERVATION</Link></li>
+                    <li><Link to='/dashboard/payment'><MdPayments /> PAYMENT HISTORY</Link></li>
+                    <li><Link to='/dashboard/mycarts'><FaShoppingCart /> MY CART</Link></li>
+                    <li><Link to='/dashboard/review'><FaRev /> REVIEW</Link></li>
+                    <li><Link to='/dashboard/booking'><FaBookOpen /> MY BOOKING</Link></li>
+                    <div className="divider"></div>
+                    <li><Link to='/'><FaHome />HOME</Link></li>
+                    <li><Link to='/menu'><AiOutlineMenu/> MENU</Link></li>
+                    <li><Link to='/shop'><FaShoppingCart /> SHOP</Link></li>
+                    <li><Link to='/dashboard/booking'><MdOutlineContactMail/> CONTACT</Link></li>
                 </ul>
 
             </div>
